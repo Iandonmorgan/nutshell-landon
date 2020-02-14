db042b12a4b84fa6697a703d40a94b676053e82d
Landon Morgan <morganlandon@gmail.com>
2020-02-14 15:58:58 -0600

    Nutshell API, initial database.json file created, data.js with shared methods created

---
+++ src/scripts/data.js
@@ -0,0 +1,39 @@
+/* 
+Code that deals with getting the data.
+*/
+
+const apiURL = "http://localhost:8088/";
+
+const API = {
+    get(str) {
+        return fetch(apiURL + str).then(entries => entries.json());
+    },
+    save(objToSave, str) {
+        return fetch(apiURL + str, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(objToSave)
+        })
+    },
+    delete(objToDeleteId, str){
+        return fetch(`${apiURL}/${str}/${objToDeleteId}`, {
+            method: "DELETE"
+        });
+    },
+    edit(objToEditId, str){
+        return fetch(apiURL + str + "/" + objToEditId).then(entry => entry.json());
+    },
+    update(objToEdit){
+        return fetch(`${apiURL}/${objToEdit.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(objToEdit)
+        });
+    }
+}
+
+export default API;
