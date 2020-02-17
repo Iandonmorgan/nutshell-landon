70f71af7e97e43510d74b779bd941dc98d08cae6
Chase Sullins <jcsullins222@yahoo.com>
2020-02-17 13:55:46 -0600

    working on the 'save' feature

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
 import API from "./data.js";
-
+import newArticleEventListener from "./articles/articleEvents.js"
 /*
     Import all the tools into main.js that are needed to display
     the initial UI to the user. Either the login form should appear
     or the dashboard should be rendered.
 */
+
 
 API.get("messages").then(objects => console.log(objects));
 API.get("events").then(objects => console.log(objects));
